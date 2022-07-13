f1a489f3e582fe59ca61012ecec54f38a7389e3f
wilbur125 <65524382+wilbur125@users.noreply.github.com>
2022-07-13 13:33:08 -0700

    Coded promise to retrieve google geocode and used Seattle as example

--- server/api/maps.js
+++ server/api/maps.js
@@ -4,18 +4,25 @@ const {Client} = require("@googlemaps/google-maps-services-js");
 
 const client = new Client({});
 
-client
-  .elevation({
+function getGeocode() {
+  client
+  .geocode({
     params: {
-      locations: [{ lat: 45, lng: -110 }],
-      key: process.env.GOOGLE_MAPS_API_KEY
+      key: process.env.GOOGLE_MAPS_API_KEY,
+      address: 'Seattle, WA'
     },
     timeout: 1000 // milliseconds
   })
-  .then((r) => {
-    let mapRes = r.data.results[0].elevation;
-    console.log(mapRes)
+  .then(gcResponse => {
+    const str = JSON.stringify(gcResponse.data.results[0]);
+    console.log(`First result is: ${str}`);
   })
   .catch(e => {
     console.log(e);
-  });
+  })
+  .finally(() => {
+    console.log('done');
+  })
+}
+
+module.exports = getGeocode();
